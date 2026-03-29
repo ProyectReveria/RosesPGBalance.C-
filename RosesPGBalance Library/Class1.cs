9bae08244d0520dd10a9f6017982ff6b2197ea2e
Rosary Orchestra Repository (sometimes) <Brokenlevenheart@gmail.com>
2026-03-29 04:21:39 -0600

    Parche 0.0.3
    Arreglos:
    -Ahora el Array tiene definiciones previas 
    -Los Valores que se necesitan obtener ahora son bajo "Object_" en ves de "Contract" para evitar la libreria de C# con la sintaxis especifica del mismo nombre

--- RosesPGBalance Library/Class1.cs
+++ RosesPGBalance Library/Class1.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics.Contracts;
 
 namespace RosesPGBalance_Library;
 
@@ -9,13 +8,18 @@ class RPGRosesFramework
     public static float Default_float = 0f;
     public static int default_int = 0; 
     //!Variables
-    public static float Contract_Health { get; set; } = 100f;
-    public static float Contract_Player_Damage { get; set; } = Default_float; 
-    public static float Contract_Enemy_Healt { get; set; } = 100f;
+    public static float Object_Health { get; set; } = 100f;
+    public static float Object_Player_Damage { get; set; } = Default_float; 
+    public static float Object_Enemy_Healt { get; set; } = 100f;
 
     //?Data definitions
 
     //?Memory Slots for Stadistics
+    public struct Data_Storage
+    {
+        public float Health;
+        public float Player_Damage;
+    }
     public struct Stadistic_Storage
     {
         public Data_Storage[] Storage_Component;
@@ -30,12 +34,10 @@ class RPGRosesFramework
     {
         //! Definiticiones
         Stadistic_Storage NewClass_Stadistics = new Stadistic_Storage();
-        NewClass_Stadistics.Storage_Component = new Player_Class[8];
-        NewClass_Stadistics.Player_Class[1].Health = Contract_Health;
-        NewClass_Stadistics.Player_Class[1].Damage = Contract_Player_Damage;
-
-
-
+        NewClass_Stadistics.Storage_Component = new Data_Storage[8];
+        NewClass_Stadistics.Storage_Component[1].Health = Object_Health;
+            ;
+        NewClass_Stadistics.Storage_Component[1].Player_Damage = Object_Player_Damage;
 
     }
 }
